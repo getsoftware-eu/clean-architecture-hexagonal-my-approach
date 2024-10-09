a6a17ad7fc3802b7c1e14d0e890f35cf62c71c09
Eugen Fanshil <e.fanshil@betterbits.de>
2024-10-10 00:36:13 +0200

    Junit for useCase

--- src/test/java/eu/getsoftware/cleanarchitecture/application/domain/usecase/user/UserExternalClientUseCaseImplTest.java
+++ src/test/java/eu/getsoftware/cleanarchitecture/application/domain/usecase/user/UserExternalClientUseCaseImplTest.java
@@ -12,6 +12,7 @@ import eu.getsoftware.cleanarchitecture.application.port.in.user.iPortService.dt
 import eu.getsoftware.cleanarchitecture.application.port.in.user.iUseCase.IUserExternalClientUseCase;
 import eu.getsoftware.cleanarchitecture.application.port.out.user.IUserResponseDTOPortPresenter;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserExternalClientUseCaseImplTest {
 
@@ -38,14 +45,15 @@ class UserExternalClientUseCaseImplTest {
         when(userDTOToExternalClientService.createNewEntity(requestDTO)).thenReturn(new UserMappedEntity(requestDTO.name(), requestDTO.password(), LocalDateTime.now()));
 
         // when:
-        assertThrows(ResponseStatusException.class, () -> {
-            UserResponseClientDTO responseDTO = registerUseCase.registerNewUser(requestDTO);
-        });
+        assertThatThrownBy(() -> registerUseCase.registerNewUser(requestDTO)).isInstanceOf(ResponseStatusException.class);
+        
         // then:
+        verify(userDomainPersistService, times(0)).saveEntity(any(UserMappedEntity.class)); //one time: we saved created entity 
+
     }
     
     @Test
-    void givenRequestDto_whenDoubleCreation_thenIsFalse() {
+    void checkInnerMethodTimesCalled() {
         
         // given:
         UserRequestUseCaseDTO requestDTO = new UserRequestUseCaseDTO("name", "username", "123456", "-");
@@ -60,6 +68,27 @@ class UserExternalClientUseCaseImplTest {
         // then:
         assertThat(responseDTO2.isPasswordValid()).isFalse();
         assertThat(responseDTO2.name()).isEmpty();
+        
+        verify(userDomainPersistService, times(1)).saveEntity(any(UserMappedEntity.class)); //one time: we saved created entity 
+        verify(userResponseDTOPortPresenter, times(1)).prepareSuccessView(any(UserResponseClientDTO.class)); //one time: we processed the responseDto
+
+    }
+
+    @Test
+    void checkInnerArgumentValueCalled() {
+
+        // given:
+        UserRequestUseCaseDTO requestDTO = new UserRequestUseCaseDTO("name", "username", "123456", "-");
+        when(userDTOToExternalClientService.createNewEntity(requestDTO)).thenReturn(new UserMappedEntity(requestDTO.name(), requestDTO.password(), LocalDateTime.now()));
+        ArgumentCaptor<String> userRequestDtoArgumentCaptor = ArgumentCaptor.forClass(String.class); // eu: only STRING-ARGUMENTS
+
+
+        UserResponseClientDTO responseDTO = registerUseCase.registerNewUser(requestDTO);
+
+        verify(userDomainPersistService).existsByName(userRequestDtoArgumentCaptor.capture()); //eu: capture inner method STRING-ARGUMENT call
+        String capturedArgument = userRequestDtoArgumentCaptor.getValue(); //eu: value of inner called STRING-ARGUMENT ++++
+        
+        assertEquals("name", capturedArgument);
     }
     
 }
